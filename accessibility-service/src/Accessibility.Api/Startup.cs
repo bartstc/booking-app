c73f25d55dce5d24a4f24bf999a99432a7f76715
kgniazdowski <kgniazdowski1@gmail.com>
2020-11-19 20:42:48 +0100

    #8 Accessibility - applications startup separation

--- accessibility-service/src/Accessibility.Api/Startup.cs
+++ accessibility-service/src/Accessibility.Api/Startup.cs
@@ -1,21 +1,9 @@
-using Accessibility.Application.Bookings.CreateBooking;
-using Accessibility.Domain.Bookings;
-using Accessibility.Domain.SeedWork;
-using Accessibility.Infrastructure.Database;
-using Accessibility.Infrastructure.Domain;
-using Accessibility.Infrastructure.Domain.Bookings;
-using Accessibility.Infrastructure.SeedWork;
-using Accessibility.Infrastructure.Processing;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Accessibility.Infrastructure.Processing.Outbox;
-using Accessibility.Application.Configuration.Database;
+using Accessibility.Infrastructure;
 
 namespace Accessibility.Api
 {
@@ -28,21 +16,10 @@ namespace Accessibility.Api
 
         public IConfiguration Configuration { get; }
 
-        // TODO: move registrations to modules
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            services.AddDbContext<AccessibilityContext>(options =>
-                options
-                    .ReplaceService<IValueConverterSelector, StronglyTypedIdValueConverterSelector>()
-                    .UseNpgsql(Configuration.GetConnectionString("Accessibility")))
-                .AddMediatR(typeof(BookingCreatedNotification).Assembly, typeof(BookingCreatedEvent).Assembly, typeof(ProcessOutboxCommand).Assembly)
-                .AddTransient<IBookingRepository, BookingRepository>()
-                .AddTransient<IUnitOfWork, UnitOfWork>()
-                .AddTransient<IDomainEventsDispatcher, DomainEventsDispatcher>()
-                .AddHostedService<ProcessOutboxHostedService>()
-                .AddScoped<ISqlConnectionFactory>(x => new SqlConnectionFactory(Configuration.GetConnectionString("Accessibility")));
+            services.ConfigureAccessibility(Configuration.GetConnectionString("Accessibility"));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
---
+++ accessibility-service/src/Accessibility.Infrastructure/Startup.cs
@@ -0,0 +1,37 @@
+using Accessibility.Application.Bookings.CreateBooking;
+using Accessibility.Application.Configuration.Database;
+using Accessibility.Domain.Bookings;
+using Accessibility.Domain.SeedWork;
+using Accessibility.Infrastructure.Database;
+using Accessibility.Infrastructure.Domain;
+using Accessibility.Infrastructure.Domain.Bookings;
+using Accessibility.Infrastructure.Processing;
+using Accessibility.Infrastructure.Processing.Outbox;
+using Accessibility.Infrastructure.SeedWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+
+namespace Accessibility.Infrastructure
+{
+    public static class Startup
+    {
+        // TODO: split registrations into modules
+        public static IServiceCollection ConfigureAccessibility(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<AccessibilityContext>(options =>
+                options
+                    .ReplaceService<IValueConverterSelector, StronglyTypedIdValueConverterSelector>()
+                    .UseNpgsql(connectionString))
+                .AddMediatR(typeof(BookingCreatedNotification).Assembly, typeof(BookingCreatedEvent).Assembly, typeof(ProcessOutboxCommand).Assembly)
+                .AddTransient<IBookingRepository, BookingRepository>()
+                .AddTransient<IUnitOfWork, UnitOfWork>()
+                .AddTransient<IDomainEventsDispatcher, DomainEventsDispatcher>()
+                .AddHostedService<ProcessOutboxHostedService>()
+                .AddScoped<ISqlConnectionFactory>(x => new SqlConnectionFactory(connectionString));
+
+            return services;
+        }
+    }
+}
