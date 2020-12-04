809018002dc40e94bf697dd54ecfcd3395c8d29b
bartstc <bartst010395@gmail.com>
2020-12-04 18:43:13 +0100

    Remove default app controller and service

--- management-service/src/app.module.ts
+++ management-service/src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import * as ormconfig from './ormconfig';
 import { EnterpriseModule } from './modules/enterprise/enterprise.module';
 import { FacilityModule } from './modules/facilities/facility.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormconfig), EnterpriseModule, FacilityModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
