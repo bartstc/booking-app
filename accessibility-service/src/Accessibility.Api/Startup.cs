5d093ffeaab118b18e219f08c31adaf764f42ca8
kgniazdowski <kgniazdowski1@gmail.com>
2020-12-21 23:21:28 +0100

    Accessibility - swagger fix

--- accessibility-service/src/Accessibility.Api/Startup.cs
+++ accessibility-service/src/Accessibility.Api/Startup.cs
@@ -23,7 +23,8 @@ namespace Accessibility.Api
             services.ConfigureAccessibility(
                 Configuration.GetConnectionString("Accessibility"),
                 typeof(BookedNotification).Assembly)
-            .AddSwaggerGen();
+            .AddSwaggerGen(options =>
+                    options.CustomSchemaIds(x => x.FullName));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
