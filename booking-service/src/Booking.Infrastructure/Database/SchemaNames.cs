72f8cfb4bcb490a0a9d096827b068ebdc608bfde
kgniazdowski <kgniazdowski1@gmail.com>
2020-12-27 02:16:26 +0100

    #25 Booking - docker setup

--- booking-service/src/Booking.Infrastructure/Database/SchemaNames.cs
+++ booking-service/src/Booking.Infrastructure/Database/SchemaNames.cs
@@ -2,7 +2,7 @@ namespace Booking.Infrastructure.Database
 {
     internal sealed class SchemaNames
     {
-        internal const string Accessibility = "booking";
+        internal const string Booking = "booking";
         internal const string App = "app";
     }
 }
