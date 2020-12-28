29bffbee35134641c19f9089f8319825b443a97e
bartstc <bartst010395@gmail.com>
2020-12-28 11:01:05 +0100

    #29 EmployeeWebClient - configure chakra-ui, add common libraries

--- employee-web-client/src/Providers.tsx
+++ employee-web-client/src/Providers.tsx
@@ -1,12 +1,17 @@
 import React, { ReactNode } from 'react';
 import { IntlProvider } from 'react-intl';
+import { ChakraProvider } from '@chakra-ui/react';
 
 interface IProps {
   children: ReactNode;
 }
 
 const Providers = ({ children }: IProps) => {
-  return <IntlProvider locale='en'>{children}</IntlProvider>;
+  return (
+    <ChakraProvider>
+      <IntlProvider locale='en'>{children}</IntlProvider>
+    </ChakraProvider>
+  );
 };
 
 export { Providers };
