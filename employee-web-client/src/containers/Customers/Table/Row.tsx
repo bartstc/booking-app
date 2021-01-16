001e64341a75cffe30f8abab929aa2448b915a85
bartstc <bartst010395@gmail.com>
2021-01-16 21:42:59 +0100

    #50 EmployeeWebClient - remove duplications

--- employee-web-client/src/containers/Customers/Table/Row.tsx
+++ employee-web-client/src/containers/Customers/Table/Row.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Badge, Flex } from '@chakra-ui/react';
+import { useIntl } from 'react-intl';
 
 import { GridItem, TruncatedCell } from 'shared/Grid';
 import { FormattedDate } from 'shared/Date';
+import { ContactButtons } from 'shared/Contact';
 
 import { ICustomer } from 'modules/customers/types';
 import { ContactType } from 'types';
-
-import { ActionButtons } from '../ActionButtons';
 
 interface IProps {
   index: number;
@@ -15,6 +15,8 @@ interface IProps {
 }
 
 const Row = ({ index, customer }: IProps) => {
+  const { formatMessage } = useIntl();
+
   const phone = customer.contacts.find(contact => contact.type === ContactType.Phone)?.value;
   const email = customer.contacts.find(contact => contact.type === ContactType.Email)?.value;
   const url = customer.contacts.find(contact => contact.type === ContactType.Url)?.value;
@@ -35,7 +37,7 @@ const Row = ({ index, customer }: IProps) => {
         <FormattedDate value={customer.birthDate} />
       </TruncatedCell>
       <TruncatedCell justify='flex-end'>
-        <ActionButtons phone={phone} email={email} url={url} />
+        <ContactButtons contacts={customer.contacts} subject={formatMessage({ id: 'customer', defaultMessage: 'customer' })} />
       </TruncatedCell>
     </GridItem>
   );
