4f99950ea40d0fe5f45552a5a598e2afde9f7d64
bartstc <bartst010395@gmail.com>
2021-01-17 10:39:15 +0100

    #50 EmployeeWebClient - add filters for offers list

--- employee-web-client/src/containers/Employees/Panel.tsx
+++ employee-web-client/src/containers/Employees/Panel.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { mdiFilter } from '@mdi/js';
 import { useIntl } from 'react-intl';
 import { Flex, HStack } from '@chakra-ui/react';
-import { isMobileOnly } from 'react-device-detect';
 
 import { ClearFiltersIconButton, FiltersInput } from 'shared/Filters';
-import { Button, IconButton } from 'shared/Button';
-import { Icon } from 'shared/Icon';
 
 const Panel = () => {
   const { formatMessage } = useIntl();
-
-  const title = formatMessage({
-    id: 'filters',
-    defaultMessage: 'Filters',
-  });
 
   return (
     <Flex justify='space-between' w='100%' mb={{ base: 2, md: 4 }}>
@@ -25,16 +16,7 @@ const Panel = () => {
         })}...`}
       />
       <HStack>
-        {isMobileOnly ? (
-          <IconButton ml={4} title={title} variant='solid' path={mdiFilter} />
-        ) : (
-          <>
-            <ClearFiltersIconButton />
-            <Button colorScheme='gray' leftIcon={<Icon path={mdiFilter} />}>
-              {title}
-            </Button>
-          </>
-        )}
+        <ClearFiltersIconButton ml={4} />
       </HStack>
     </Flex>
   );
--- employee-web-client/src/containers/Offers/Panel/index.tsx
+++ employee-web-client/src/containers/Offers/Panel/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { mdiFilter } from '@mdi/js';
 import { useIntl } from 'react-intl';
-import { Flex, HStack } from '@chakra-ui/react';
+import { Flex, HStack, useDisclosure } from '@chakra-ui/react';
 import { isMobileOnly } from 'react-device-detect';
 
 import { ClearFiltersIconButton, FiltersInput } from 'shared/Filters';
 import { Button, IconButton } from 'shared/Button';
 import { Icon } from 'shared/Icon';
+import { FiltersModal } from './FiltersModal';
 
 const Panel = () => {
   const { formatMessage } = useIntl();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const title = formatMessage({
     id: 'filters',
@@ -26,12 +28,13 @@ const Panel = () => {
         filterName='name'
       />
       <HStack>
+        <FiltersModal isOpen={isOpen} onClose={onClose} />
         {isMobileOnly ? (
-          <IconButton ml={4} title={title} variant='solid' path={mdiFilter} />
+          <IconButton ml={4} title={title} variant='solid' path={mdiFilter} onClick={onOpen} />
         ) : (
           <>
             <ClearFiltersIconButton />
-            <Button colorScheme='gray' leftIcon={<Icon path={mdiFilter} />}>
+            <Button colorScheme='gray' onClick={onOpen} leftIcon={<Icon path={mdiFilter} />}>
               {title}
             </Button>
           </>
