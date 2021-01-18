27450269923da0408a13efad0d15a06585ac4195
kgniazdowski <kgniazdowski1@gmail.com>
2021-01-18 23:03:08 +0100

    #36 Booking - OfferCreated event integration test

--- booking-service/src/Booking.Application/Facilities/IntegrationEvents/Events/OfferCreated.cs
+++ booking-service/src/Booking.Application/Facilities/IntegrationEvents/Events/OfferCreated.cs
@@ -4,6 +4,15 @@ namespace Booking.Application.Facilities.IntegrationEvents.Events
 {
     public class OfferCreated
     {
+        public OfferCreated(Guid id, Guid facilityId, decimal price, string currency, short duration)
+        {
+            this.Id = id;
+            this.FacilityId = facilityId;
+            this.Price = price;
+            this.Currency = currency;
+            this.Duration = duration;
+
+        }
         public Guid Id { get; }
         public Guid FacilityId { get; }
         public decimal Price { get; }
---
+++ booking-service/src/tests/Booking.IntegrationTests/Facilities/FacilityTests.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Threading.Tasks;
+using Booking.Api;
+using Booking.Application.Facilities.IntegrationEvents.EventHandling;
+using Booking.Application.Facilities.IntegrationEvents.Events;
+using Booking.Domain.SharedKernel;
+using Booking.Infrastructure.Database;
+using MassTransit;
+using MassTransit.Testing;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Booking.IntegrationTests.Facilities
+{
+    public class FacilityTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> factory;
+
+        public FacilityTests(WebApplicationFactory<Startup> factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task InMemoryTestHarness_OfferCreatedPublished_OfferPersisted()
+        {
+            using (var scope = factory.Services.CreateScope())
+            {
+                var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+                var ctx = scope.ServiceProvider.GetRequiredService<BookingContext>();
+                var harness = new InMemoryTestHarness();
+                var consumerHarness = harness.Consumer<OfferCreatedConsumer>(() => new OfferCreatedConsumer(mediator));
+
+                var offerId = new OfferId(Guid.NewGuid());
+                var facilityId = new FacilityId(Guid.NewGuid());
+                var price = 50;
+                var currency = "PLN";
+                short duration = 60;
+
+                await harness.Start();
+                try
+                {
+                    await harness.InputQueueSendEndpoint.Send(new OfferCreated(
+                        offerId.Value,
+                        facilityId.Value,
+                        price,
+                        currency,
+                        duration
+                    ));
+
+                    Assert.True(await harness.Consumed.Any<OfferCreated>());
+                    Assert.True(await consumerHarness.Consumed.Any<OfferCreated>());
+                    Assert.False(await harness.Published.Any<Fault<OfferCreated>>());
+
+                    var persistedOffer = await ctx.Offers.FirstOrDefaultAsync(o => o.Id == offerId);
+
+                    Assert.NotNull(persistedOffer);
+                    Assert.Equal(facilityId, persistedOffer.FacilityId);
+                    Assert.Equal(price, persistedOffer.Price);
+                    Assert.Equal(currency, persistedOffer.Currency);
+                    Assert.Equal(duration, persistedOffer.Duration);
+                }
+                finally
+                {
+                    await harness.Stop();
+                }
+            }
+        }
+    }
+}
