ba54663376aa8cea24f70bde251bab946fa4f291
bartstc <bartst010395@gmail.com>
2021-02-10 20:32:32 +0100

    Management - create logger service

--- management-service/src/app.module.ts
+++ management-service/src/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
 
+import { DatabaseModule } from './database';
+import { ConfigModule } from './config';
+import { LoggerModule } from './logger';
 import { EnterpriseModule } from './modules/enterprise/enterprise.module';
 import { FacilityModule } from './modules/facilities/facility.module';
 import { CustomerModule } from './modules/customers/customer.module';
-import { DatabaseModule } from './database';
-import { ConfigModule } from './config';
 
 @Module({
   imports: [
     DatabaseModule,
+    ConfigModule,
+    LoggerModule,
     EnterpriseModule,
     FacilityModule,
     CustomerModule,
-    ConfigModule,
   ],
 })
 export class AppModule {}
--- management-service/src/logger/logger.service.ts
+++ management-service/src/logger/logger.service.ts
@@ -1,45 +1,48 @@
 import { Injectable, Logger } from '@nestjs/common';
 
+import { ILoggerService } from './ILoggerService';
+
 enum ELogLevel {
-  debug,
-  info,
-  warn,
-  error,
+  Debug,
+  Info,
+  Warn,
+  Error,
 }
 
-// @Injectable()
-// export class LoggerService extends Logger {
-//   private readonly _currentLevel: ELogLevel = ELogLevel[config.get<ILogSettings>('LOGGER_SETTINGS').level];
-//
-//   constructor(private readonly _context?: string) {
-//     super(_context);
-//   }
-//
-//   public log(message: unknown, context?: string) {
-//     if (this.isValidLevel(ELogLevel.debug)) {
-//       Logger.log(JSON.stringify(message), context || this._context);
-//     }
-//   }
-//
-//   public info(message: unknown, context?: string) {
-//     if (this.isValidLevel(ELogLevel.info)) {
-//       Logger.log(JSON.stringify(message), context || this._context);
-//     }
-//   }
-//
-//   public warn(message: unknown, context?: string) {
-//     if (this.isValidLevel(ELogLevel.warn)) {
-//       Logger.warn(JSON.stringify(message), context || this._context);
-//     }
-//   }
-//
-//   public error(message: unknown, trace?: string, context?: string) {
-//     if (this.isValidLevel(ELogLevel.error)) {
-//       Logger.error(JSON.stringify(message), trace, context || this._context);
-//     }
-//   }
-//
-//   private isValidLevel(level: ELogLevel): boolean {
-//     return level >= this._currentLevel;
-//   }
-// }
+@Injectable()
+export class LoggerService extends Logger implements ILoggerService {
+  private readonly _currentLevel: ELogLevel =
+    (process.env.LOGGER_LEVEL as any) ?? ELogLevel.Info;
+
+  constructor(private readonly _context?: string) {
+    super(_context);
+  }
+
+  public log(message: unknown, context?: string) {
+    if (this.isValidLevel(ELogLevel.Debug)) {
+      Logger.log(JSON.stringify(message), context || this._context);
+    }
+  }
+
+  public info(message: unknown, context?: string) {
+    if (this.isValidLevel(ELogLevel.Info)) {
+      Logger.log(JSON.stringify(message), context || this._context);
+    }
+  }
+
+  public warn(message: unknown, context?: string) {
+    if (this.isValidLevel(ELogLevel.Warn)) {
+      Logger.warn(JSON.stringify(message), context || this._context);
+    }
+  }
+
+  public error(message: unknown, trace?: string, context?: string) {
+    if (this.isValidLevel(ELogLevel.Error)) {
+      Logger.error(JSON.stringify(message), trace, context || this._context);
+    }
+  }
+
+  private isValidLevel(level: ELogLevel): boolean {
+    return level >= this._currentLevel;
+  }
+}
