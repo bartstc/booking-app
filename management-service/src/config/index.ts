365009a56f4f92929c58115bdca3046840c362f7
bartstc <bartst010395@gmail.com>
2021-02-13 18:00:02 +0100

    Management - configure cors

--- management-service/src/config/index.ts
+++ management-service/src/config/index.ts
@@ -1,3 +1,5 @@
 export { ConfigModule } from './config.module';
 export { ConfigService } from './config.service';
 export { IConfigService } from './IConfigService';
+export { RequestManager } from './request.manager';
+export { corsOptionsDelegate } from './cors.option';
