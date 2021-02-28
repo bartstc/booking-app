244fcb673fdb0bb6a2c751a9fffd61833483c3d5
bartstc <bartst010395@gmail.com>
2021-02-28 15:35:36 +0100

    #65 EmployeeWebClient - add confirmation for activate/deactivate employee

--- employee-web-client/src/modules/employees/application/activateEmployee/ActivateEmployeeIconButton.tsx
+++ employee-web-client/src/modules/employees/application/activateEmployee/ActivateEmployeeIconButton.tsx
@@ -1,34 +1,47 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
+import { useDisclosure } from '@chakra-ui/react';
 import { mdiLockOpenVariantOutline } from '@mdi/js';
 
 import { IconButton, IconButtonProps } from 'shared/Button';
+import { Confirm } from 'shared/Confirm';
 
 import { useActivateEmployeeNotification } from './useAactivateEmployeeNotification';
+import { useActivateEmployee } from '../../infrastructure/command';
 
 interface IProps extends Omit<IconButtonProps, 'title' | 'onClick'> {
-  onClick: () => Promise<void>;
+  facilityId: string;
+  employeeId: string;
 }
 
-const ActivateEmployeeIconButton = ({ onClick, ...props }: IProps) => {
+const ActivateEmployeeIconButton = ({ facilityId, employeeId, ...props }: IProps) => {
   const { formatMessage } = useIntl();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { showFailureNotification, showSuccessNotification } = useActivateEmployeeNotification();
+  const [activate, isActivating] = useActivateEmployee(facilityId, employeeId);
 
   return (
-    <IconButton
-      title={formatMessage({ id: 'activate-employee', defaultMessage: 'Activate employee' })}
-      path={mdiLockOpenVariantOutline}
-      onClick={() => {
-        onClick()
-          .then(() => {
-            showSuccessNotification();
-          })
-          .catch(() => {
-            showFailureNotification();
-          });
-      }}
-      {...props}
-    />
+    <>
+      <Confirm
+        isOpen={isOpen}
+        isLoading={isActivating}
+        onClose={onClose}
+        onConfirm={() => {
+          activate()
+            .then(() => showSuccessNotification())
+            .catch(() => showFailureNotification())
+            .finally(() => onClose());
+        }}
+        status='info'
+      />
+      <IconButton
+        isLoading={isActivating}
+        title={formatMessage({ id: 'activate-employee', defaultMessage: 'Activate employee' })}
+        path={mdiLockOpenVariantOutline}
+        onClick={onOpen}
+        {...props}
+      />
+    </>
   );
 };
 
