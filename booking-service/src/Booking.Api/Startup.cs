6ebbf9fa6bed718839333097585c8e9a79e5b391
kgniazdowski <kgniazdowski1@gmail.com>
2021-03-01 17:23:20 +0100

    Booking - swagger in production environment

--- booking-service/src/Booking.Api/Startup.cs
+++ booking-service/src/Booking.Api/Startup.cs
@@ -42,9 +42,10 @@ namespace Booking.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Booking.Api v1"));
             }
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Booking.Api v1"));
 
             app.UseHttpsRedirection();
 
