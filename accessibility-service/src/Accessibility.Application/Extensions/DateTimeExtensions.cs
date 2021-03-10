3da3df2c838f05fe10b6a35f0b75a6ab012051bc
kgniazdowski <kgniazdowski1@gmail.com>
2021-03-10 19:03:59 +0100

    Accessibility - fixing overlapping periods detection method

--- accessibility-service/src/Accessibility.Application/Extensions/DateTimeExtensions.cs
+++ accessibility-service/src/Accessibility.Application/Extensions/DateTimeExtensions.cs
@@ -8,6 +8,6 @@ namespace Accessibility.Application.Extensions
             new DateTime(date.Ticks - date.Ticks % roundTicks, date.Kind);
 
         public static bool HasCommonPeriod(this (DateTime start, DateTime end) period1, (DateTime start, DateTime end) period2) =>
-            (period1.start >= period2.start && period1.start < period2.end) || (period1.end > period2.start && period1.end <= period2.end);
+            period1.start < period2.end && period2.start < period1.end;
     }
 }
