9c1d93022a6184cabd6d2ccdb4cfec7724593364
bartstc <bartst010395@gmail.com>
2021-07-08 20:18:07 +0200

    EmployeeWebClient - update env from auth service

--- employee-web-client/src/modules/auth/presentation/AuthButton.tsx
+++ employee-web-client/src/modules/auth/presentation/AuthButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { LogoutButton } from './LogoutButton';
 import { LoginButton } from './LoginButton';
@@ -6,6 +6,16 @@ import { useAuthContextSelector } from '../application';
 
 const AuthButton = () => {
   const isAuthenticated = useAuthContextSelector(state => state.isAuthenticated);
+  const getUser = useAuthContextSelector(state => state.getUser);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await getUser();
+      console.log(user);
+    };
+
+    fetchUser();
+  }, []);
 
   return isAuthenticated() ? <LogoutButton /> : <LoginButton />;
 };
