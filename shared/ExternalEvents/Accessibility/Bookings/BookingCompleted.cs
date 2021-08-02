1efe85c9ae92971843130175a2fa3ca83e9de858
kgniazdowski <kgniazdowski1@gmail.com>
2021-08-02 19:00:46 +0200

    #101 Accessibility - changing completed booking integration event structure

---
+++ shared/ExternalEvents/Accessibility/Bookings/BookingCompleted.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using ExternalEvents.Accessibility.Bookings.Dtos;
+
+namespace ExternalEvents.Accessibility.Bookings
+{
+    public class BookingCompleted
+    {
+        public BookingCompleted(Guid publicCustomerId, IEnumerable<BookedRecord> bookedRecords)
+        {
+            PublicCustomerId = publicCustomerId;
+            BookedRecords = bookedRecords;
+        }
+
+        public Guid PublicCustomerId { get; }
+        public IEnumerable<BookedRecord> BookedRecords { get; }
+    }
+
+    public class BookedRecord
+    {
+        public BookedRecord(Offer offer, Facility facility, Employee employee, DateTime date, short duration, Guid bookedRecordId, BookedRecordStatus status)
+        {
+            Offer = offer;
+            Facility = facility;
+            Employee = employee;
+            Date = date;
+            Duration = duration;
+            BookedRecordId = bookedRecordId;
+            Status = status;
+        }
+
+        public Offer Offer { get; }
+        public Facility Facility { get; }
+        public Employee Employee { get; }
+        public DateTime Date { get; }
+        public short Duration { get; }
+        public Guid BookedRecordId { get; }
+        public BookedRecordStatus Status { get; }
+    }
+
+    public enum BookedRecordStatus
+    {
+        Fulfilled,
+        Canceled,
+        NotRealized
+    }
+}
