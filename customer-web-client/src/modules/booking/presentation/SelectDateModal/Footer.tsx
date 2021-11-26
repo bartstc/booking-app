6dd18ee7dbeffce29685e97ef1d4165f2445fccb
bartstc <bartst010395@gmail.com>
2021-11-26 16:41:56 +0100

    CustomerWebClient - add form loading state to submit button

--- customer-web-client/src/modules/booking/presentation/SelectDateModal/Footer.tsx
+++ customer-web-client/src/modules/booking/presentation/SelectDateModal/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
 import { ModalFooter } from "@chakra-ui/react";
+import { useFormContext } from "react-hook-form";
 
 import { Button } from "shared/Button";
 
@@ -10,12 +11,17 @@ interface IProps {
 }
 
 const Footer = ({ onClose, isDisabled }: IProps) => {
+  const {
+    formState: { isSubmitting },
+  } = useFormContext();
+
   return (
     <ModalFooter>
       <Button
         isDisabled={isDisabled}
         type="submit"
         form="booking-form"
+        isLoading={isSubmitting}
         colorScheme="blue"
       >
         <FormattedMessage id="accept" defaultMessage="Accept" />
