6913463dae7cd98c31a19f884f3fc429b7bb6fde
bartstc <bartst010395@gmail.com>
2022-10-31 18:29:34 +0100

    EmployeeWebClient - make table styles more consistent

--- employee-web-client/src/pages/Offers/index.tsx
+++ employee-web-client/src/pages/Offers/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { VStack } from '@chakra-ui/react';
 
 import { PageContainer } from 'shared/Layout/Page';
 import { OffersCollection } from 'modules/offers/presentation';
 
 import { Header } from './Header';
 import { OffersCollectionToolbox } from '../../modules/offers/presentation/OffersCollection/OffersCollectionToolbox';
+import { CollectionContainer } from '../../shared/Collection';
 
 const Offers = () => {
   return (
     <PageContainer>
       <Header />
-      <VStack w='100%' maxW='1200px' pb={{ base: 4, md: 10 }}>
+      <CollectionContainer>
         <OffersCollectionToolbox />
         <OffersCollection />
-      </VStack>
+      </CollectionContainer>
     </PageContainer>
   );
 };
